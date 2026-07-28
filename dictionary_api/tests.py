0f5e8ac2e17b96384652fd0cf0ca5eb2b384279c
cabral <cabralnogueiraadenilson@gmail.com>
2026-07-28 14:57:21 -0400

    Testes models

--- dictionary_api/tests.py
+++ dictionary_api/tests.py
@@ -11,11 +11,55 @@ class UserModelTeste(TestCase):
         pass
 
 
+    def test_user_str_returns_username(self):
+        user = User.objects.create_user(
+            username='user',
+            password='12345'
+        )
+        self.assertEqual(str(user), 'user')
+
+
+    def test_create_superuser(self):
+        admin = User.objects.create_superuser(
+            username='admin',
+            password='12345'
+        )
+
+        self.assertTrue(admin.is_superuser)
+        self.assertTrue(admin.is_staff)
+
+
 
 class WordModelTest(TestCase):
     
     def test_word_is_created_successfully(self):
         pass
+
+
+    def test_word_str_returns_word(self):
+        user = User.objects.create_superuser(
+            username='admin',
+            password='12345'
+        )
+        word = Word.objects.create(
+            word='word',
+            meaning='...',
+            created_by=user
+        )
+
+        self.assertEqual(str(word), 'word')
+
+    
+    def test_automatic_fields_are_set(self):
+        user = User.objects.create_superuser(
+            username='admin',
+            password='12345'
+        )
+        word = Word.objects.create(
+            word='word',
+            meaning='...',
+            created_by=user
+        )
 
 
 # Testes dos serializers
