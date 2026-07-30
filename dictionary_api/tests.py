4845940086797f55f7ef6f4e3e0bf95db0af0c70
cabral <cabralnogueiraadenilson@gmail.com>
2026-07-30 14:56:16 -0400

    Last models tests

--- dictionary_api/tests.py
+++ dictionary_api/tests.py
@@ -1,15 +1,16 @@
 from django.test import TestCase
 from .models import User, Word
 from django.db.utils import IntegrityError
+from django.contrib.auth import get_user_model
+
+import time
+
+Usuario = get_user_model()
 
 
 # Testes dos models
 
 class UserModelTeste(TestCase):
-    
-    def test_user_is_created_successfully(self):
-        pass
-
 
     def test_user_str_returns_username(self):
         user = User.objects.create_user(
@@ -31,10 +32,6 @@ class UserModelTeste(TestCase):
 
 
 class WordModelTest(TestCase):
-    
-    def test_word_is_created_successfully(self):
-        pass
-
 
     def test_word_str_returns_word(self):
         user = User.objects.create_superuser(
@@ -60,6 +57,50 @@ class WordModelTest(TestCase):
             meaning='...',
             created_by=user
         )
+        old_updated = word.updated_at
+        time.sleep(1)
+        word.meaning = 'novo significado'
+        word.save()
+
+        self.assertIsNotNone(word.created_at)
+        self.assertGreater(word.updated_at, old_updated)
+
+
+    def test_word_must_be_unique(self):
+
+        user = User.objects.create_superuser(
+            username='admin',
+            password='12345'
+        )
+        word = Word.objects.create(
+            word='word',
+            meaning='...',
+            created_by=user
+        )
+
+        with self.assertRaises(IntegrityError):
+            word = Word.objects.create(
+                word='word',
+                meaning='...',
+                created_by=user
+            )
+
+
+    def test_created_by_becomes_null_when_deleted(self):
+
+        user = User.objects.create_superuser(
+            username='admin',
+            password='12345'
+                )
+        word = Word.objects.create(
+            word='word',
+            meaning='...',
+            created_by=user
+                )
+
+        user.delete()
+        word.refresh_from_db()
+        self.assertIsNone(word.created_by)
 
 
 # Testes dos serializers
